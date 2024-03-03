8b96cafd4fb51beeb56854eb64cdb7f64920c17d
PhanMinhSon <sonpm@hivelab.com>
2024-03-03 23:29:41 +0700

    feat: config router

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+import Home from '~/pages/Home'
+import Following from '~/pages/Following'
+import Profile from '~/pages/Profile'
+
+// public routes
+const publicRoutes = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/following',
+    component: Following,
+  },
+  {
+    path: '/profile',
+    component: Profile,
+  },
+]
+
+const privateRoutes = []
+
+export { publicRoutes, privateRoutes }
